abe1e2dcb6cf08a9bfa9bec6b832d593f0c53842
aelaboussi <aelaboussi@ayaline.com>
2026-09-04 17:31:46 +0100

    fix ads strategy

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
             async
             src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${siteConfig.adsenseClientId}`}
             crossOrigin="anonymous"
-            strategy="afterInteractive"
+            strategy="beforeInteractive"
           />
         )}
         <Header />
